fe2c6f98b8a3c40e502545e0c48d3fa0f7cb935f
GuilhermeBarroso-Sys <guilhermebarrosodeoliveira@outlook.com>
2021-02-14 10:05:31 -0300

    class

---
+++ Class.js
@@ -0,0 +1,17 @@
+class hero {
+    constructor(){
+        this.name = '',
+        this.health = 0,
+        this.atk = 0;
+    }
+
+    Atk() {
+        console.log("Atk!");
+    }
+    jump() {
+        console.log("Jumping!");
+    }
+    walk() {
+        console.log("Walking!");
+    }
+}
